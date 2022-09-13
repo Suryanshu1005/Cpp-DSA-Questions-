35040e48719079440d1e39f056d3d88b9314a44c
Suryanshu Tiwari <tshuryanshu@gmail.com>
2022-09-14 01:52:33 +0530

    new commit

--- Arrays/SubarrayWithGivenSum.cpp
+++ Arrays/SubarrayWithGivenSum.cpp
@@ -10,17 +10,17 @@ class Solution{
         long long sum = 0;
         int p=0;
         for(int i=0;i<n;i++){
-            if(s==0) return {-1};
-            sum += arr[i];
-            while(sum > s){
-                sum -= arr[p];
+            if(s==0) return {-1};              //agar sum ki value zero hai to -1 return kar denge
+            sum += arr[i];                     //nhi toh array ka sum krke sum me store kr lenge
+            while(sum > s){                    //jab tak sum s se bada hai tb tk loop chalao
+                sum -= arr[p];                 //aur har baar sum mese arr[p] ko subtract kr do
                 p++;
             }
-            if(sum == s){
-                return {p+1, i+1};
+            if(sum == s){                       //jaise hi sum == s ho jaye
+                return {p+1, i+1};              //values ko return kr do
             }
         }
-        return {-1};
+        return {-1};                    
     }
 };
 
